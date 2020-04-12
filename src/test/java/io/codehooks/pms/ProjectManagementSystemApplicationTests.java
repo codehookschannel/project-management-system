4a771029362b174158305955113daf2748b723d0
Vijay Karchi <karchi.vijay999@gmail.com>
2020-04-12 14:42:29 +0530

    initial commit

---
+++ src/test/java/io/codehooks/pms/ProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codehooks.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
