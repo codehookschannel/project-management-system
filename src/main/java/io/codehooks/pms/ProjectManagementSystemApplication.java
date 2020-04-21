45325647fdd1a0484544aa40c94c3ff1be684a75
Code-Hooks <codehookschannel@gmail.com>
2020-04-22 01:58:49 +0530

    Testing removed.

--- src/main/java/io/codehooks/pms/ProjectManagementSystemApplication.java
+++ src/main/java/io/codehooks/pms/ProjectManagementSystemApplication.java
@@ -2,6 +2,8 @@ package io.codehooks.pms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 public class ProjectManagementSystemApplication {
@@ -9,5 +11,6 @@ public class ProjectManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementSystemApplication.class, args);
 	}
+
 
 }
